00bbf2725cf8800d59bc264b6c3e462edaa9b7e8
sohaib-gujjar <43603836+sohaib-gujjar@users.noreply.github.com>
2021-10-24 22:52:10 +0200

    Update app.test.ts
    test

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,16 +2,12 @@ import request from 'supertest'
 import { app } from './index';
 
 describe('GET /calculus', () => {
-      
-    it('should return 200 & valid response', async done => {
-      request(app)
-        .get(`/calculus?query=[MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk=]`)
-        //.expect('Content-Type', /json/)
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err)
-          expect(res.body).toMatchObject({'error': false, 'result': -132.88888888888889})
-          done()
-        })
-    })
+  test("should return 200 & valid response", async () => {
+    await request(app)
+      .get("/calculus?query=[MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk=]")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toMatchObject({ error: false, result: -132.88888888888889 })
+      })
   })
+})
